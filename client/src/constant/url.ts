9eba7b3f48c0cc018b89db6616e50a3d3cf8bdc8
developerjrts <developer.jrts@gmail.com>
2026-08-14 16:35:35 +0530

    Endpoints added

--- client/src/constant/url.ts
+++ client/src/constant/url.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const url = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://learnx-sii0.onrender.com/api",
     withCredentials: true
 })
--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,12 @@ const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:5173/",
+        "https://learn-x-gamma.vercel.app/",
+        "https://learn-x-gamma.vercel.app",
+    ],
     credentials: true
 }));
 app.use(cookieParser());
