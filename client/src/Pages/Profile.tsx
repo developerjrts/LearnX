d8c0e110de130a81f2a12691a82f3254be8059b9
developerjrts <developer.jrts@gmail.com>
2026-08-15 18:01:50 +0530

    Removed unwanted imports

--- client/src/Pages/Profile.tsx
+++ client/src/Pages/Profile.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios, {isAxiosError} from "axios"
 import type { User } from '@/types/types'
 import { Avatar, Skeleton, Box, Button, Card, Typography } from '@mui/material'
